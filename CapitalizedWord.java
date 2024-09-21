21fcc82081c4a06ba707912548b8e1a12eaec865
zunaidaalam <zunaid.aalam6194@gmail.com>
2024-09-21 12:36:24 +0530

    Update Printable Statement

--- CapitalizedWord.java
+++ CapitalizedWord.java
@@ -3,6 +3,7 @@ package JavaProgram;
 public class CapitalizedWord {
     public static int CapitalWord(String inputString){
 
+        System.out.println("Capitalized Word");
         int counter = 0;
         for(int i = 0; i<inputString.length();i++){
             if(inputString.charAt(i)>= 'A' && inputString.charAt(i)<='Z'){
